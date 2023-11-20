fd69e92ac0648bfa19742f103f704872d0ab65e6
jc-hiroto <pohaocha@andrew.cmu.edu>
2023-11-20 01:35:25 -0800

    Refactor Heading component and useMDXComponents function

--- components/mdx/Heading.tsx
+++ components/mdx/Heading.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import { Text, As } from '@chakra-ui/react'
+import React from "react";
+import { Text, As } from "@chakra-ui/react";
 
 interface HeadingProps {
-    children: React.ReactNode;
-    level: As;
-    size: string;
-    weight: string;
-    family?: string;
+  children: React.ReactNode;
+  level: As;
+  size: string;
+  weight: string;
+  family?: string;
 }
 
-function Heading({ children, level, size, weight, family="mono" }: HeadingProps) {
-    return (
-        <Text as={level} fontSize={size} fontWeight={weight} fontFamily={family}>{children}</Text>
-    );
+function Heading({
+  children,
+  level,
+  size,
+  weight,
+  family = "mono",
+}: HeadingProps) {
+  return (
+    <Text as={level} fontSize={size} fontWeight={weight} fontFamily={family}>
+      {children}
+    </Text>
+  );
 }
 
 export default Heading;
