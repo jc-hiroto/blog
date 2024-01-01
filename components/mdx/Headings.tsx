cdfd609d2a60c454433a495fab7168dfb18502c4
jc-hiroto <pohaocha@andrew.cmu.edu>
2024-01-01 22:45:48 +0800

    Run Prettier.

--- components/mdx/Headings.tsx
+++ components/mdx/Headings.tsx
@@ -1,7 +1,7 @@
 import { FiLink } from "react-icons/fi";
 
 interface HeadingProps {
-  id: string
+  id: string;
 }
 
 const copyHeadingLink = (id: string) => {
@@ -11,18 +11,21 @@ const copyHeadingLink = (id: string) => {
 };
 
 export function H1({ id, ...props }: HeadingProps) {
-    return (
-      <div className="flex flex-row justify-start items-center mt-12 mb-4 group cursor-pointer" onClick={() => copyHeadingLink(id)}>
-        <FiLink className="text-gray-800 hidden sm:block sm:text-xl sm:mr-4 sm:-ml-[36px] group-hover:text-gray-300 transition-all duration-300" />
-        <h1
-            id={id}
-            className="sm:text-5xl text-4xl font-bold text-gray-100"
-            {...props}
-        />
-        <hr className="ml-4 hidden sm:block border-gray-700 flex-grow" />
-        <FiLink className="text-gray-800 text-lg ml-4 sm:hidden group-hover:text-gray-300 transition-all duration-300" />
-      </div>
-    )
+  return (
+    <div
+      className="flex flex-row justify-start items-center mt-12 mb-4 group cursor-pointer"
+      onClick={() => copyHeadingLink(id)}
+    >
+      <FiLink className="text-gray-800 hidden sm:block sm:text-xl sm:mr-4 sm:-ml-[36px] group-hover:text-gray-300 transition-all duration-300" />
+      <h1
+        id={id}
+        className="sm:text-5xl text-4xl font-bold text-gray-100"
+        {...props}
+      />
+      <hr className="ml-4 hidden sm:block border-gray-700 flex-grow" />
+      <FiLink className="text-gray-800 text-lg ml-4 sm:hidden group-hover:text-gray-300 transition-all duration-300" />
+    </div>
+  );
 }
 
 export function H2({ id, ...props }: HeadingProps) {
@@ -37,7 +40,7 @@ export function H2({ id, ...props }: HeadingProps) {
       <hr className="hidden sm:block ml-4 border-gray-800 flex-grow" />
       <FiLink className="text-gray-800 text-lg ml-4 sm:hidden group-hover:text-gray-300 transition-all duration-300" />
     </div>
-  )
+  );
 }
 
 export function H3({ id, ...props }: HeadingProps) {
@@ -47,7 +50,7 @@ export function H3({ id, ...props }: HeadingProps) {
       className="text-3xl font-bold text-gray-100 mt-4 mb-2"
       {...props}
     />
-  )
+  );
 }
 
 export function H4({ id, ...props }: HeadingProps) {
@@ -57,7 +60,7 @@ export function H4({ id, ...props }: HeadingProps) {
       className="text-2xl font-bold text-gray-100 mt-2 mb-1"
       {...props}
     />
-  )
+  );
 }
 
 export function H5({ id, ...props }: HeadingProps) {
@@ -67,11 +70,11 @@ export function H5({ id, ...props }: HeadingProps) {
       className="text-xl font-bold text-gray-100 mt-2 mb-1"
       {...props}
     />
-  )
+  );
 }
 
 export function H6({ id, ...props }: HeadingProps) {
   return (
     <h6 id={id} className="text-lg font-bold text-white mt-2 mb-1" {...props} />
-  )
+  );
 }
