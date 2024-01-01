0f8730cca50ab2b4fa6c3e5c305092e1d8ee0d2a
jc-hiroto <pohaocha@andrew.cmu.edu>
2024-01-02 00:14:30 +0800

    Change header font style.

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,18 +4,17 @@ import { FiExternalLink } from "react-icons/fi";
 function Header() {
   return (
     <div className="w-full h-[64px] px-8 flex flex-row items-center justify-between">
-      <Link href="/" className="font-mono text-xl font-semibold text-gray-400">
+      <Link href="/" className="font-mono text-lg sm:text-xl font-semibold text-gray-400">
         blog.
       </Link>
       <hr className="w-[80%] mx-4 border-gray-600" />
       <Link
         href="https://jchiroto.dev"
-        className="font-mono text-sm text-gray-400"
+        className="font-mono text-md text-gray-400"
         isExternal
       >
         <div className="flex flex-row align-middle">
           <p className="flex flex-row">jchiroto.dev </p>
-          <FiExternalLink className="my-auto ml-2" />
         </div>
       </Link>
     </div>
